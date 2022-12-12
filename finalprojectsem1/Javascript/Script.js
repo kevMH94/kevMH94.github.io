ac630a6bcce7e10b4b3efd8cdafb53fd8ee84a08
kevMH94 <116730890+kevMH94@users.noreply.github.com>
2022-12-12 20:06:16 +0000

    created javascript folder

--- finalprojectsem1/Javascript/Script.js
+++ finalprojectsem1/Javascript/Script.js
@@ -4,10 +4,14 @@ function newsletter(){
     var button = document.getElementById('button-addon2').value;
 
     if(button.clicked = 'true' && input != ''){
-    alert("You have successfully signed up for our newsletter! Thanks a lot!");
+        if(input.indexOf('@')>-1){
+            alert("You have successfully signed up for our newsletter! Thanks a lot!");
+        } else {
+            alert("Please include an '@' sign into your email address " + input + " is missing an '@'");
+        } 
     } else {
         alert("Please add an email address to sign up!");
-    }
+        }
 }
 //Code by Kevin
 function showDescription(id){
